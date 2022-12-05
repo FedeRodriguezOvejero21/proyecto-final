0ccd6bcb828c53631973bc4a71e7a95046b9228e
Federico Rodriguez Ovejero <federodriguezovejero@gmail.com>
2022-12-05 20:21:29 -0300

    Agrego nuevo buscar

---
+++ ejemplo/forms.py
@@ -0,0 +1,3 @@
+from django import forms
+class Buscar(forms.Form):
+    nombre = forms.CharField(max_length=100)
--- ejemplo/views.py
+++ ejemplo/views.py
@@ -1,8 +1,7 @@
 from django.shortcuts import render
 from ejemplo.models import Familiar
-from datetime import date
-from datetime import datetime
-
+from ejemplo.forms import Buscar
+from django.views import View
 
 def index(request):
     return render(request, "ejemplo/saludar.html")
@@ -38,3 +37,20 @@ def buscar(request):
 def mostrar_familiares(request):
     lista_familiares = Familiar.objects.all()
     return render(request, "ejemplo/familiares.html", {"lista_familiares": lista_familiares})
+
+class BuscarFamiliar(View):
+    form_class = Buscar
+    template_name = 'ejemplo/buscar.html'
+    initial = {"nombre":""}
+    def get(self, request):
+        form = self.form_class(initial=self.initial)
+        return render(request, self.template_name, {'form':form})
+    def post(self, request):
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            nombre = form.cleaned_data.get("nombre")
+            lista_familiares = Familiar.objects.filter(nombre__icontains=nombre).all() 
+            form = self.form_class(initial=self.initial)
+            return render(request, self.template_name, {'form':form, 
+                                                        'lista_familiares':lista_familiares})
+        return render(request, self.template_name, {"form": form})
