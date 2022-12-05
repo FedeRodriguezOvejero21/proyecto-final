5eab174fc9b2e012d66a6849a03a0055abf4898f
Federico Rodriguez Ovejero <federodriguezovejero@gmail.com>
2022-12-05 20:53:18 -0300

    Nuevos cambios

--- ejemplo/models.py
+++ ejemplo/models.py
@@ -1,11 +1,11 @@
 from django.db import models
-from datetime import datetime
+
 
 class Familiar(models.Model):
     nombre = models.CharField(max_length=100)
     direccion = models.CharField(max_length=200)
     numero_pasaporte = models.IntegerField()
-    fecha_nacimiento= models.CharField(max_length=100)
+    fecha_nacimiento= models.DateField(input_format=['%d-%m-%Y'])
 
     def __str__(self):
       return f"{self.nombre}, {self.numero_pasaporte}, {self.id}, {self.fecha_nacimiento}"
