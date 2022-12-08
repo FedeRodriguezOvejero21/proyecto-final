751e6af6e06e7d78d64acb02083ee60eac91c756
Federico Rodriguez Ovejero <federodriguezovejero@gmail.com>
2022-12-07 23:09:10 -0300

    Intento modificar fecha

--- ejemplo/models.py
+++ ejemplo/models.py
@@ -5,7 +5,7 @@ class Familiar(models.Model):
     nombre = models.CharField(max_length=100)
     direccion = models.CharField(max_length=200)
     numero_pasaporte = models.IntegerField()
-    fecha_nacimiento= models.DateField()
+    fecha_nacimiento= models.DateField(["%m-%d-%Y"])
 
     def __str__(self):
       return f"{self.nombre}, {self.numero_pasaporte}, {self.id}, {self.fecha_nacimiento}"
@@ -13,5 +13,4 @@ class Familiar(models.Model):
 
 class Dummy(models.Model):
     nombre=models.CharField(max_length=100)
- 
-   
+
