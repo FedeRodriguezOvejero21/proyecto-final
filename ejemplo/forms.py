3a97b7385127736eabd35aa4643b683a70367699
Federico Rodriguez Ovejero <federodriguezovejero@gmail.com>
2022-12-11 21:26:45 -0300

    intento x

--- ejemplo/forms.py
+++ ejemplo/forms.py
@@ -5,7 +5,7 @@ class Buscar(forms.Form):
   nombre = forms.CharField(max_length=100)
   direccion = forms.CharField(max_length=200) 
   numero_pasaporte = forms.IntegerField() 
-  fecha_nacimiento= models.DateField()
+  fecha_nacimiento= forms.DateField()
 
 class FamiliarForm(forms.ModelForm):
   class Meta:
