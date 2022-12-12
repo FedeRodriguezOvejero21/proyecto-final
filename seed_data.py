de1a7385cf422589bb1299fb9c8a1e93e6ffe1ae
Federico Rodriguez Ovejero <federodriguezovejero@gmail.com>
2022-12-12 19:08:56 -0300

    modificacion de familiares error

--- seed_data.py
+++ seed_data.py
@@ -1,5 +1,5 @@
 from ejemplo.models import Familiar
-Familiar(nombre="Federico", direccion="Avenida la Plata 486", numero_pasaporte=123123, fecha_nacimiento= "1992/07/21").save()
+Familiar(nombre="Federico", direccion="Avenida la Plata 486", numero_pasaporte=123123, fecha_nacimiento= "21-07-1992").save()
 Familiar(nombre="Maria", direccion="Cardoso 6", numero_pasaporte=890890, fecha_nacimiento= "1955/08/16").save()
 Familiar(nombre="Horacio", direccion="Avenida Rivadavia 8521", numero_pasaporte=345345, fecha_nacimiento= "1960/04/24").save()
 Familiar(nombre="Rosa", direccion="Avenida Rivadavia 8521", numero_pasaporte=567567, fecha_nacimiento= "1920/03/21").save()
