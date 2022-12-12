1aed4e657dde700e45e545e8af5d046021270baa
Federico Rodriguez Ovejero <federodriguezovejero@gmail.com>
2022-12-11 23:28:29 -0300

    reemplazo guion por barra en fecha

--- seed_data.py
+++ seed_data.py
@@ -1,6 +1,6 @@
 from ejemplo.models import Familiar
-Familiar(nombre="Federico", direccion="Avenida la Plata 486", numero_pasaporte=123123, fecha_nacimiento= "1992-07-21").save()
-Familiar(nombre="Maria", direccion="Cardoso 6", numero_pasaporte=890890, fecha_nacimiento= "1955-08-16").save()
-Familiar(nombre="Horacio", direccion="Avenida Rivadavia 8521", numero_pasaporte=345345, fecha_nacimiento= "1960-04-24").save()
-Familiar(nombre="Rosa", direccion="Avenida Rivadavia 8521", numero_pasaporte=567567, fecha_nacimiento= "1920-03-21").save()
+Familiar(nombre="Federico", direccion="Avenida la Plata 486", numero_pasaporte=123123, fecha_nacimiento= "1992/07/21").save()
+Familiar(nombre="Maria", direccion="Cardoso 6", numero_pasaporte=890890, fecha_nacimiento= "1955/08/16").save()
+Familiar(nombre="Horacio", direccion="Avenida Rivadavia 8521", numero_pasaporte=345345, fecha_nacimiento= "1960/04/24").save()
+Familiar(nombre="Rosa", direccion="Avenida Rivadavia 8521", numero_pasaporte=567567, fecha_nacimiento= "1920/03/21").save()
 print("Se cargo con éxito los usuarios de pruebas")
